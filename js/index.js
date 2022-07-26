958e3c018b316ac2b4e1df29eeab3bd729b9a45d
Gary Taylor <garycharlestaylor@outlook.com>
2022-07-26 17:18:00 +0100

    added projects info toggle

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const infoBtns = document.querySelectorAll('.info')
 const moreInfoBtn = document.querySelector('.more-info');
 const lessInfoBtn = document.querySelector('.less-info');
 const aboutText = document.querySelectorAll('.section-text-about');
+const projectHeadings = document.querySelectorAll('.project-heading');
 
 function infoBtnClick(){
     if(moreInfoBtn.innerText ==='More Info'){
@@ -27,6 +28,26 @@ navToggle.addEventListener('click', () => {
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open')
+    })
+})
+
+projectHeadings.forEach(heading => {
+    
+    heading.addEventListener('click', () => {
+        let projectText = heading.nextElementSibling;
+
+        if(projectText.style.display === 'none' || projectText.style.display ===''){
+            projectText.style.display = 'block';
+            heading.firstElementChild.classList.remove('fa-caret-right');
+            heading.firstElementChild.classList.add('fa-caret-down')
+        } else {
+            projectText.style.display ='none';
+            heading.firstElementChild.classList.remove('fa-caret-down');
+            heading.firstElementChild.classList.add('fa-caret-right');
+            
+        }
+
+
     })
 })
 
