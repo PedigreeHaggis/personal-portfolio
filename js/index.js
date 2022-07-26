83bb385033a46a75bb2081ef099505baf74c1bba
Gary Taylor <garycharlestaylor@outlook.com>
2022-07-26 15:18:26 +0100

    Button all js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,23 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav-link');
+const infoBtns = document.querySelectorAll('.info')
 const moreInfoBtn = document.querySelector('.more-info');
 const lessInfoBtn = document.querySelector('.less-info');
 const aboutText = document.querySelectorAll('.section-text-about');
 
+function infoBtnClick(){
+    if(moreInfoBtn.innerText ==='More Info'){
+        moreInfoBtn.style.display = 'none';
+        moreInfoBtn.innerText = ''
+        aboutText.forEach(x => x.style.display = 'grid');
+        lessInfoBtn.style.display = 'grid';
+    } else if(lessInfoBtn.innerText === 'Less Info' && moreInfoBtn.style.display === 'none'){
+        lessInfoBtn.style.display = 'none';
+        moreInfoBtn.innerText = 'More Info'
+        aboutText.forEach(x => x.style.display ='none');
+        moreInfoBtn.style.display ='grid';
+    }
+}
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -15,14 +29,6 @@ navLinks.forEach(link => {
     })
 })
 
-function infoBtnClick(){
-    if(moreInfoBtn.innerText ==='More Info'){
-        moreInfoBtn.style.display = 'none';
-        aboutText.forEach(x => x.style.display = 'grid');
-        lessInfoBtn.style.display = 'grid';
-    } else if(lessInfoBtn.innerText === 'Less Info' && moreInfoBtn.style.display === 'none'){
-        lessInfoBtn.style.display = 'none';
-        aboutText.forEach(x => x.style.display ='none');
-        moreInfoBtn.style.display ='grid';
-    }
-}
+infoBtns.forEach(btn => btn.addEventListener('click', infoBtnClick))
+
+
