3d2ca17c496241083584acc1220717fd46eeba05
Gary Taylor <garycharlestaylor@outlook.com>
2022-07-26 15:02:12 +0100

    added btn for more info

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav-link');
+const moreInfoBtn = document.querySelector('.more-info');
+const lessInfoBtn = document.querySelector('.less-info');
+const aboutText = document.querySelectorAll('.section-text-about');
 
 
 navToggle.addEventListener('click', () => {
@@ -12,4 +15,14 @@ navLinks.forEach(link => {
     })
 })
 
-
+function infoBtnClick(){
+    if(moreInfoBtn.innerText ==='More Info'){
+        moreInfoBtn.style.display = 'none';
+        aboutText.forEach(x => x.style.display = 'grid');
+        lessInfoBtn.style.display = 'grid';
+    } else if(lessInfoBtn.innerText === 'Less Info' && moreInfoBtn.style.display === 'none'){
+        lessInfoBtn.style.display = 'none';
+        aboutText.forEach(x => x.style.display ='none');
+        moreInfoBtn.style.display ='grid';
+    }
+}
